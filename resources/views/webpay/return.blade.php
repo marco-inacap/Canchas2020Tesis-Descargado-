cf3599994b599fcdfa237d52c0678dc1bc5196a3
marco-inacap <65793535+marco-inacap@users.noreply.github.com>
2020-07-13 20:41:15 -0400

    Update return.blade.php

--- resources/views/webpay/return.blade.php
+++ resources/views/webpay/return.blade.php
@@ -1,24 +1,13 @@
-{{--  <form method="POST" action="{{$result->urlRedirection}}"  id="return-form">
-        @csrf 
-        <input type="hidden" name="token_ws" value="{{$result->token}}">
-    </form>
+@extends('layout')
 
-    <script>
-    
-    document.getElementById('return-form').submit();
-        
-    </script> --}}
-
-{{-- <meta name="csrf-token" content="{{ csrf_token() }}">
-
-<form method="POST" action="{{ $response->urlRedirection }}" >
-    @csrf
-    <input type="hidden" name="token_ws" value="{{$token}}">
-
-    OK
-</form> --}}
-ok
-<form action="{{ $result->urlRedirection }}" method="post">
-    @csrf
-    <input type="hidden" name="token_ws" value="{{ $tokenWs }}">
-</form>
+@section('content')
+<section class="pages container">
+    <div class="page page-contact">
+        <form action="{{ $response->urlRedirection }}" method="post">
+            @csrf
+            <input type="hidden" name="token_ws" value="{{ $token }}">
+            <button target="_blank" class="btn btn-default"><i class="fa fa-print"></i>IR AL DETALLE DE LA COMPRA</button>
+        </form>
+    </div>
+</section>
+@endsection
