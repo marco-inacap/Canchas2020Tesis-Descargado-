3e6fb697043500b856591e8642cf5df94a9ff3f6
marco-inacap <65793535+marco-inacap@users.noreply.github.com>
2020-07-13 20:22:04 -0400

    Update Reserva.php

--- app/Reserva.php
+++ app/Reserva.php
@@ -10,9 +10,33 @@ use Illuminate\Support\Facades\DB;
 
 class Reserva extends Model
 {
+    public const STATUS_PENDING_PAYMENT = 0;
+    // Webpay Plus Normal status
+    public const STATUS_WP_NORMAL_INIT_SUCCESS = 11;
+    public const STATUS_WP_NORMAL_INIT_ERROR = 12;
+    public const STATUS_WP_NORMAL_FINISH_SUCCESS = 13;
+    public const STATUS_WP_NORMAL_FINISH_INVALID = 14;
+    public const STATUS_WP_NORMAL_FINISH_ERROR = 15;
+    public const STATUS_WP_NORMAL_FINISH_ABORT = 16;
+    public const STATUS_WP_NORMAL_FINISH_TIMEOUT = 17;
+    public const STATUS_WP_NORMAL_FINISH_FORM_FAULT = 18;
+
+    public const STATUS_DESC = [
+        self::STATUS_PENDING_PAYMENT => 'Pendiente de pago',
+        self::STATUS_WP_NORMAL_INIT_SUCCESS => 'Pago con Webpay Normal iniciado',
+        self::STATUS_WP_NORMAL_INIT_ERROR => 'Error al inciar pago con Webpay Normal',
+        self::STATUS_WP_NORMAL_FINISH_SUCCESS => 'Pago con Webpay Normal finalizado',
+        self::STATUS_WP_NORMAL_FINISH_INVALID => 'Error de validación al finalizar pago con Webpay Normal',
+        self::STATUS_WP_NORMAL_FINISH_ERROR => 'Error al finalizar pago con Webpay Normal',
+        self::STATUS_WP_NORMAL_FINISH_ABORT => 'Pago con Webpay Normal abortado',
+        self::STATUS_WP_NORMAL_FINISH_TIMEOUT => 'Pago con Webpay Normal cancelado por timeout',
+        self::STATUS_WP_NORMAL_FINISH_FORM_FAULT => 'Pago con Webpay Normal con error en formulario de pago',
+    ];
+
+
     protected $fillable = [
 
-        'fecha', 'hora_inicio', 'hora_fin', 'cancha_id', 'user_id'
+        'fecha', 'hora_inicio', 'hora_fin', 'cancha_id','complejo_id','user_id','total'
     ];
     
     public function user()
