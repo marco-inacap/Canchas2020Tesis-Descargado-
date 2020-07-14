b4124a3aa44388d0d3ec23a163a5e4dd01bc84e9
marco-inacap <65793535+marco-inacap@users.noreply.github.com>
2020-07-13 23:40:09 -0400

    Update ReservaCanchaController.php

--- app/Http/Controllers/ReservaCanchaController.php
+++ app/Http/Controllers/ReservaCanchaController.php
@@ -4,15 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Cancha;
-use App\Complejo;
-use App\Estado;
 use App\User;
 use App\Reserva;
 use App\Horario;
 use App\Transaction;
+use App\Respuesta;
 use Transbank\Webpay\Configuration;
 use Transbank\Webpay\Webpay;
 use Transbank\Webpay\WebPayNormal;
+use Carbon\Carbon;
 use Auth;
 
 class ReservaCanchaController extends Controller
@@ -183,17 +183,17 @@ class ReservaCanchaController extends Controller
         }
     }
 
-    public function init_webpay(Request $request, $id)
+    public function init_webpay(Request $request, $id) 
     {
         $canchas = Cancha::findOrFail($id); 
         $usuarioauth = Auth()->user()->id;
         $reserva = Reserva::where('user_id',auth()->id())->take(1)->first();
 
 
-        if ($reserva->status != Reserva::STATUS_PENDING_PAYMENT)
+        /* if ($reserva->status != Reserva::STATUS_PENDING_PAYMENT)
         {
             return redirect()->route('pages.home');
-        }
+        } */
 
         $db_transaction = new Transaction;
         $db_transaction->reserva()->associate($reserva);
@@ -210,33 +210,140 @@ class ReservaCanchaController extends Controller
                 $returnUrl,
                 $finalUrl    
         );
-        if (is_array($response))
+        /* if (is_array($response))
         {
             $db_transaction->error = html_entity_decode($response['detail']);
             $db_transaction->save();
             $reserva->status = Reserva::STATUS_WP_NORMAL_INIT_ERROR;
             $reserva->save();
             return redirect()->route('pages.home');
-        }   
+        } */   
 
         $db_transaction->token = $response->token;
         $db_transaction->save();
         $reserva->status = Reserva::STATUS_WP_NORMAL_INIT_SUCCESS;
         $reserva->save();
+
+       
         
-        return view('webpay.index',compact('response'));         
+        return view('webpay.index',compact('response','reserva','db_transaction','canchas'));         
 
     }
 
     public function return_webpay(Request $request)
     {
+        $token = $request->input('token_ws');
+        $db_transaction = Transaction::where('token', $token)->first();
+        /* if (!$db_transaction || $db_transaction->reserva->status != Reserva::STATUS_WP_NORMAL_INIT_SUCCESS)
+        {
+            return redirect()->route('pages.home');
+        } */
 
+        $transaction = self::getTransaction();
+        $response = $transaction->getTransactionResult($token);
+
+        $db_response = new Respuesta();
+        $db_response->transaction()->associate($db_transaction);
+
+        /* if (is_array($response))
+        {
+            $db_response->error = html_entity_decode($response['detail']);
+            $db_response->save();
+            $db_transaction->reserva->status = Reserva::STATUS_WP_NORMAL_FINISH_ERROR;
+            $db_transaction->reserva->save();
+            return redirect()->route('pages.home');
+        } */
+
+        $db_response->buy_order = $response->buyOrder;
+        $db_response->session_id = $response->sessionId;
+        $db_response->accounting_date = Carbon::parse($response->accountingDate)->format('Y-m-d','America/Santiago');
+        $db_response->transaction_date = Carbon::parse($response->transactionDate)->format('Y-m-d H:i:s','America/Santiago');
+        $db_response->vci = $response->VCI;
+        $db_response->card_number = $response->cardDetail->cardNumber;
+        $db_response->amount = $response->detailOutput->amount;
+        $db_response->authorization_code = $response->detailOutput->authorizationCode;
+        $db_response->payment_type_code = $response->detailOutput->paymentTypeCode;
+        $db_response->response_code = $response->detailOutput->responseCode;
+
+        /* if (isset($response->detailOutput->responseDescription))
+        {
+        $db_response->response_description = $response->detailOutput->responseDescription;
+        } */
+
+        $db_response->shares_number = $response->detailOutput->sharesNumber;
+        $db_response->save();
+
+        /*  if (!$db_response->is_valid)
+        {
+            $db_transaction->reserva->status = Reserva::STATUS_WP_NORMAL_FINISH_INVALID;
+            $db_transaction->reserva->save();
+            return redirect()->route('pages.home');
+        }  */
+
+        $db_transaction->reserva->status = Reserva::STATUS_WP_NORMAL_FINISH_SUCCESS;
+        $db_transaction->reserva->save();
+
+        /* return redirect($response->url,$response->token); */
         
+        return view('webpay.return', compact('response', 'token'));
 
     }
 
-    public function final(Request $request)
+    public function final_webpay(Request $request)
     {
+        $token = $request->input('token_ws', null) ?? $request->input('TBK_TOKEN', null);
+        $session_id = $request->input('TBK_ID_SESION', null);
+        $buy_order = $request->input('TBK_ORDEN_COMPRA', null);
+
+        $db_transaction = null;
+        if ($token)
+        {
+            $db_transaction = Transaction::where('token', $token)->first();
+        }
+
+        elseif ($buy_order && $session_id)
+        {
+            $buy_order_exploded = explode('_', $buy_order);
+            $created_timestamp = array_pop($buy_order_exploded);
+            $db_transaction = Transaction::where([
+                ['reserva_id', $session_id],
+                ['created_at', Carbon::createFromTimestamp($created_timestamp)]
+            ])->first();
+        }
+
+        /* if (!$db_transaction)
+        {
+        return redirect()->route('pages.home');
+        }  */
+
+        switch ($db_transaction->reserva->status)
+        {
+            case Reserva::STATUS_WP_NORMAL_FINISH_SUCCESS:
+                $response = $db_transaction->response;
+                return view('webpay.final', compact('response'));
+            case Reserva::STATUS_WP_NORMAL_INIT_SUCCESS:
+                $db_response = new Respuesta;
+                $db_response->transaction()->associate($db_transaction);
+                $db_response->buy_order = $buy_order;
+                $db_response->session_id = $session_id;
+                $db_response->save();
+
+                /* switch (count($request->all()))
+                {
+                    case 2:
+                        $db_transaction->reserva->status = Reserva::STATUS_WP_NORMAL_FINISH_TIMEOUT;
+                        break;
+                    case 3:
+                        $db_transaction->reserva->status = Reserva::STATUS_WP_NORMAL_FINISH_ABORT;
+                        break;
+                    case 4:
+                        $db_transaction->reserva->status = Reserva::STATUS_WP_NORMAL_FINISH_FORM_FAULT;
+                        break;    
+                }
+                $db_transaction->reserva->save();
+                default:
+                return redirect()->route('pages.home');   */
+        }
 
     }
 
