fe845bd4945dfefb1730bee463b6ced25fb1ec27
marco-inacap <65793535+marco-inacap@users.noreply.github.com>
2020-07-18 18:29:20 -0400

    Update ComplejoTableSeeder.php

--- database/seeds/ComplejoTableSeeder.php
+++ database/seeds/ComplejoTableSeeder.php
@@ -17,18 +17,24 @@ class ComplejoTableSeeder extends Seeder
         $complejo = new Complejo();
         $complejo->nombre = "Osorno Soccer";
         $complejo->ubicacion = "Osorno";
+        $complejo->latitude = "-40.568803";
+        $complejo->longitude = "-73.1423372";
         $complejo->telefono = "123456789";
         $complejo->save();
 
         $complejo = new Complejo();
         $complejo->nombre = "Canchas Matices";
         $complejo->ubicacion = "Osorno";
+        $complejo->latitude = "-40.5708959";
+        $complejo->longitude = "-73.1551774";
         $complejo->telefono = "123456789";
         $complejo->save();
 
         $complejo = new Complejo();
         $complejo->nombre = "Canchas Real";
         $complejo->ubicacion = "Osorno";
+        $complejo->latitude = "-40.5707995";
+        $complejo->longitude = "-73.1550085";
         $complejo->telefono = "123456789";
         $complejo->save();
 
