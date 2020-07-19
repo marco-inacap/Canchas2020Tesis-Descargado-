897eb5596d0291b7005e6d71b35fb984dc204ab5
marco-inacap <65793535+marco-inacap@users.noreply.github.com>
2020-07-19 02:10:08 -0400

    Update ComplejoController.php

--- app/Http/Controllers/Admin/ComplejoController.php
+++ app/Http/Controllers/Admin/ComplejoController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Complejo;
 use App\Http\Requests\StoreComplejoRequest;
+use Illuminate\Support\Facades\Storage;
 
 class ComplejoController extends Controller
 {
@@ -13,18 +14,15 @@ class ComplejoController extends Controller
     {
         $user = Auth()->user();
 
-        if($user->hasRole('Admin'))
-        {
+        if ($user->hasRole('Admin')) {
             $complejos = Complejo::all();
-            
-        }else{
-            
-            $complejos = $user->complejo()->get(); 
-            
+        } else {
+
+            $complejos = $user->complejo()->get();
         }
 
 
-        return view('admin.complejos.index',compact('complejos'));
+        return view('admin.complejos.index', compact('complejos'));
     }
     public function create()
     {
@@ -35,43 +33,48 @@ class ComplejoController extends Controller
     {
         $data = $request->validate([
             'nombre' => 'required|string|max:30|unique:complejos',
-            'imagen' => 'required',
+            'imagen' => 'required|image|mimes:jpg,jpeg,png,gif,svg|max:2048',
             'ubicacion' => 'required|string|max:255',
-            'telefono' => 'required|min:8|max:10|unique:complejos',
-            'latitude'=>'nullable',
-            'longitude'=>'nullable',
+            'telefono' => 'required|min:8|max:13|unique:complejos',
+            'latitude' => 'nullable',
+            'longitude' => 'nullable',
         ]);
 
-        if($request->file('imagen')){
-            $path = Storage::disk('public')->put('complejos',$request->file('imagen'));
+        Complejo::create([
 
-            $data->fill(['imagen'=> asset($path)])->save();
-        }
+            'nombre' => request('nombre'),
+            'ubicacion' => request('ubicacion'),
+            'latitude' => request('latitude'),
+            'longitude' => request('longitude'),
+            'telefono' => request('telefono'),
+            'url_imagen' => 'storage/' . request()->file('imagen')->store('complejos', 'public'),
 
-        $complejo = Complejo::create($data);
+        ]);
 
         return redirect()->route('admin.complejo.index')->withFlash('El Complejo ha sido creado');
-
     }
     public function edit(Complejo $complejo)
     {
 
-        return view('admin.complejos.edit',compact('complejo'));
+        return view('admin.complejos.edit', compact('complejo'));
     }
     public function update(StoreComplejoRequest $request, Complejo $complejo)
     {
-        
-        
-        $complejo->update($request->validated());
 
-        return redirect()->route('admin.complejo.edit', $complejo)->with('flash','El Complejo ha sido actualizado');
+        if (request()->hasFile('imagen')) {
 
+            $complejo->update([
+                $request->validated(),
+                'url_imagen' => 'storage/' . request()->file('imagen')->store('complejos', 'public'),
+            ]);
+        }
+
+        return redirect()->route('admin.complejo.edit', $complejo)->with('flash', 'El Complejo ha sido actualizado');
     }
     public function destroy(Complejo $complejo)
     {
         $complejo->delete();
-        
-        return redirect()->route('admin.complejo.index')->with('flash','El Complejo ha sido eliminado');
-    }
 
+        return redirect()->route('admin.complejo.index')->with('flash', 'El Complejo ha sido eliminado');
+    }
 }
