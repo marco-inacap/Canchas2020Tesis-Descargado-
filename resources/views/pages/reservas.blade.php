157d14a93a9da28da419b0cefb52a2166ba03f43
marco-inacap <65793535+marco-inacap@users.noreply.github.com>
2020-07-19 21:43:20 -0400

    Update reservas.blade.php

--- resources/views/pages/reservas.blade.php
+++ resources/views/pages/reservas.blade.php
@@ -9,6 +9,7 @@
         <div class="divider-2" style="margin:25px 0;"></div>
         <div class="form-contact">
             <form action="#">
+                @if ($reservas->count() === 1)
                 <table class="table table-striped">
                     <thead>
                         <tr>
@@ -18,14 +19,14 @@
                             <th scope="col">Complejo</th>
                             <th scope="col">Estado</th>
                             <th scope="col">Total</th>
-                            
                         </tr>
                     </thead>
                     <tbody>
                         @foreach ($reservas as $reserva)
                         <tr>
                             <td>{{Carbon\Carbon::parse($reserva->fecha)->isoFormat('D - MMMM - YYYY')}}</td>
-                            <td>{{  Carbon\Carbon::parse($reserva->hora_inicio)->isoFormat('HH:mm ') }} - {{  Carbon\Carbon::parse($reserva->hora_fin)->isoFormat('HH:mm a') }}</td>
+                            <td>{{  Carbon\Carbon::parse($reserva->hora_inicio)->isoFormat('HH:mm ') }} -
+                                {{  Carbon\Carbon::parse($reserva->hora_fin)->isoFormat('HH:mm a') }}</td>
                             <td>{{$reserva->cancha->nombre}}</td>
                             <td>{{$reserva->cancha->complejo->nombre}}</td>
                             <td>{{\App\reserva::STATUS_DESC[$reserva->status]}}</td>
@@ -33,17 +34,20 @@
                             <td>
                                 <a href="{{route('detalle.reserva', $reserva)}}">ver</a>
                                 <a target="_blank" href="{{route('detalle.reserva.download', $reserva)}}">Pdf</a>
-                            </td> 
+                            </td>
                         </tr>
                         @endforeach
                     </tbody>
                 </table>
+                @elseif($reservas->count() < 1 )
+                <p>Aún no tienes reservas, que esperas!</p>
+                @endif
+
             </form>
         </div>
-        
     </div>
 </section>
-    
+
 @endsection
 @push('styles')
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
