42b4ded26e309d1ccd7e2bd00a553b7d4a5b02b1
marco-inacap <65793535+marco-inacap@users.noreply.github.com>
2020-07-22 18:15:54 -0400

    Create ComplejoPolicy.php

---
+++ app/Policies/ComplejoPolicy.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Complejo;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Spatie\Permission\Models\Role;
+
+class ComplejoPolicy
+{
+    use HandlesAuthorization;
+
+    public function before($user)
+    {
+        if($user->hasRole('Admin'))
+        {
+            return true;
+
+        }
+    }
+
+    public function view(User $user , Complejo $complejo)
+    {
+        return $user->hasRole('Admin') || $user->hasPermissionTo('View Complejo');
+    }
+
+    public function create(User $user )
+    {
+        return $user->hasRole('Admin') || $user->hasPermissionTo('Create Complejo');
+    }
+
+    public function update(User $user )
+    {
+        return $user->hasRole('Admin') || $user->hasPermissionTo('Update Complejo');
+    }
+
+    public function delete(User $user )
+    {
+        return $user->hasRole('Admin') || $user->hasPermissionTo('Delete Complejo');
+    }
+}
