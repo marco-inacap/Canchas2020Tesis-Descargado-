9f00faf330210e81107e95aa88d228ff75426193
marco-inacap <65793535+marco-inacap@users.noreply.github.com>
2020-07-22 18:15:23 -0400

    Update PermissionsController.php

--- app/Http/Controllers/Admin/PermissionsController.php
+++ app/Http/Controllers/Admin/PermissionsController.php
@@ -4,17 +4,17 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Spatie\Permission\Models\Permission; 
+use Spatie\Permission\Models\Permission;
 use App\Providers\AuthServiceProviderGate\Gate;
 
 class PermissionsController extends Controller
 {
     public function index()
     {
-        /* $this->authorize('view', Permission::class); */
-          $this->authorize('view', new Permission);   
 
-        return view('admin.permissions.index',[
+        $this->authorize('view', new Permission);
+
+        return view('admin.permissions.index', [
 
             'permissions' => Permission::all(),
 
@@ -22,24 +22,21 @@ class PermissionsController extends Controller
     }
     public function edit(Permission $permission)
     {
-        $this->authorize('update',$permission);   
+        $this->authorize('update', $permission);
 
-        return view('admin.permissions.edit',[
+        return view('admin.permissions.edit', [
 
             'permission' => $permission
-        ]); 
-
+        ]);
     }
     public function update(Request $request, Permission $permission)
     {
-         $this->authorize('update',$permission);   
+        $this->authorize('update', $permission);
 
         $data = $request->validate(['display_name' => 'required']);
 
         $permission->update($data);
 
         return redirect()->route('admin.permissions.edit', $permission)->withFlash('El Permiso fue actualizado exitosamente');
-
     }
-
 }
