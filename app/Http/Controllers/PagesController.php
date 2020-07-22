93269d14282ffc4c2c69501d8ff7a3bd0acc0d10
marco-inacap <65793535+marco-inacap@users.noreply.github.com>
2020-07-22 18:15:31 -0400

    Update PagesController.php

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,47 +9,87 @@ use App\Reserva;
 use App\Transaction;
 use App\Respuesta;
 use Auth;
+use Transbank\Webpay\Configuration;
+use Transbank\Webpay\Webpay;
+use Transbank\Webpay\WebPayNormal;
 
 class PagesController extends Controller
 {
+
+    private static function getTransaction(): WebPayNormal
+    {
+        $urlRepository = "https://raw.githubusercontent.com/TransbankDevelopers/transbank-webpay-credenciales/master/";
+        $dirFiles = "integracion/Webpay%20Plus%20-%20CLP/597020000540";
+
+        $contentPublicCert = file_get_contents($urlRepository . $dirFiles . ".crt");
+        $contentPrivateKey = file_get_contents($urlRepository . $dirFiles . ".key");
+
+        $configuration = new Configuration();
+        $configuration->setCommerceCode(597020000540);
+        $configuration->setEnvironment("INTEGRACION");
+        $configuration->setPrivateKey($contentPrivateKey);
+        $configuration->setPublicCert($contentPublicCert);
+        $webpay = new Webpay($configuration);
+        return $webpay->getNormalTransaction();
+    }
     public function spa()
     {
         return view('pages.spa');
     }
-    
+
     public function home()
     {
         $canchas = Cancha::latest('created_at')->paginate(5);
 
-        return view ('pages.home',compact('canchas'));
+        return view('pages.home', compact('canchas'));
     }
 
     public function contacto()
     {
         return view('pages.contacto');
-
     }
 
-    public function reservas(){
+    public function reservas()
+    {
 
-        $reservas = Reserva::where('user_id',auth()->id())->get();
+        $reservas = Reserva::where('user_id', auth()->id())->get();
 
-        return view('pages.reservas',compact('reservas'));
+
+
+        return view('pages.reservas', compact('reservas'));
     }
 
-    public function detalle(Reserva $reserva){
+    public function detalle(Reserva $reserva)
+    {
 
-    $transaction = Transaction::where('reserva_id',$reserva->id)->get();  
-    
-    
-    foreach ($transaction as $valor) {
-        $responses = Respuesta::where('transaction_id',$valor->id)->get(); 
-            
-        
-    }
-   
+        $transaction = Transaction::where('reserva_id', $reserva->id)->get();
 
-    
-    return view('pages.reserva-user', compact('reserva','responses'));    
+
+        foreach ($transaction as $valor) {
+            $responses = Respuesta::where('transaction_id', $valor->id)->get();
+        }
+
+        $db_transaction = new Transaction;
+        $db_transaction->reserva()->associate($reserva);
+        $db_transaction->save();
+
+        $transaction = self::getTransaction();
+        $returnUrl =  route('webpay.return');
+        $finalUrl = route('webpay.final');
+
+        $response = $transaction->initTransaction(
+            $db_transaction->reserva->total,
+            $db_transaction->buy_order,
+            $db_transaction->reserva->id,
+            $returnUrl,
+            $finalUrl
+        );
+
+        $db_transaction->token = $response->token;
+        $db_transaction->save();
+        $reserva->status = Reserva::STATUS_WP_NORMAL_INIT_SUCCESS;
+        $reserva->save();
+
+        return view('pages.reserva-user', compact('reserva', 'responses','response'));
     }
 }
