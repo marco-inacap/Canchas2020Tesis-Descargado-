aedbe660afb988c06772ff7740080e0ab3a60ff8
marco-inacap <65793535+marco-inacap@users.noreply.github.com>
2020-07-28 17:31:58 -0400

    Update canchas.blade.php

--- resources/views/admin/ganancias/canchas.blade.php
+++ resources/views/admin/ganancias/canchas.blade.php
@@ -4,23 +4,21 @@
 
 <h1>
     Canchas 
-     <small>Seleccione una Cancha</small>
-   </h1>
-   <ol class="breadcrumb">
-     <li><a href="{{route('dashboard')}}"><i class="fa fa-dashboard"></i> Inicio</a></li>
-     
-   </ol>
+    <small>Seleccione una Cancha</small>
+</h1>
+<ol class="breadcrumb">
+    <li><a href="{{route('dashboard')}}"><i class="fa fa-dashboard"></i> Inicio</a></li>
+    <li class="active">Ganancias</li>
+    <li class="active">{{$complejo->nombre}}</li>
+  </ol>
 
 @endsection
 
-
 @section('content')
-
 
 @foreach ($canchas as $cancha)
     <div class="col-md-3">
         <div class="box box-primary">
-            
             <div class="box-header with-border"></div>
                 <img class="profile-user-img img-responsive img-circle" style="width:120px; height:120px;"  src="{{ url($cancha->photos->first()->url) }}"alt="{{$cancha->nombre}}" onerror="this.src='/img/logo.png';">
             <div class="box-body">  
@@ -33,7 +31,6 @@
                 <div class="progress-bar progress-bar-green"  style="width: {{ 10/100 * $cancha->total_visitas}}%"></div>
                 </div>
             </div>
-            
             <ul class="list-group list-group-unbordered">
             <li class="list-group-item">              
                 <b>Nº de Reservas</b> <a class="pull-right text-blue">{{count($cancha->reservas)}}</a>
@@ -43,9 +40,8 @@
             </li>
             <li class="list-group-item">
                 <b>Ganancias Total </b> <a class="pull-right"> <span class="description-percentage text-red"><i class="fa fa-caret-up"></i> $ {{number_format($cancha->reservas->sum('total'), 0, ',', '.' ) }}</span></a> 
-            <a href="{{route('admin.ganancias.lista', $cancha)}}" class="btn btn-primary btn-block"><b>Ver más</b></a>
-            </div>
-            
+            </div>  
+            <a href="{{route('admin.ganancias.lista', $cancha)}}" class="btn bg-lightblue btn-flat margin"><b>Ver más</b></a>
         </div>
     </div>
     @endforeach
