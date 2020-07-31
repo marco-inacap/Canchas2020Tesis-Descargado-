74e74a59eef9149d87f947013ffaa3a5439c9f4e
marco-inacap <65793535+marco-inacap@users.noreply.github.com>
2020-07-31 00:09:46 -0400

    Update PagesController.php

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -17,22 +17,7 @@ use Transbank\Webpay\WebPayNormal;
 class PagesController extends Controller
 {
 
-    private static function getTransaction(): WebPayNormal
-    {
-        $urlRepository = "https://raw.githubusercontent.com/TransbankDevelopers/transbank-webpay-credenciales/master/";
-        $dirFiles = "integracion/Webpay%20Plus%20-%20CLP/597020000540";
-
-        $contentPublicCert = file_get_contents($urlRepository . $dirFiles . ".crt");
-        $contentPrivateKey = file_get_contents($urlRepository . $dirFiles . ".key");
-
-        $configuration = new Configuration();
-        $configuration->setCommerceCode(597020000540);
-        $configuration->setEnvironment("INTEGRACION");
-        $configuration->setPrivateKey($contentPrivateKey);
-        $configuration->setPublicCert($contentPublicCert);
-        $webpay = new Webpay($configuration);
-        return $webpay->getNormalTransaction();
-    }
+    
     public function spa()
     {
         return view('pages.spa');
@@ -90,27 +75,7 @@ class PagesController extends Controller
             $responses = Respuesta::where('transaction_id', $valor->id)->get();
         }
 
-        $db_transaction = new Transaction;
-        $db_transaction->reserva()->associate($reserva);
-        $db_transaction->save();
 
-        $transaction = self::getTransaction();
-        $returnUrl =  route('webpay.return');
-        $finalUrl = route('webpay.final');
-
-        $response = $transaction->initTransaction(
-            $db_transaction->reserva->total,
-            $db_transaction->buy_order,
-            $db_transaction->reserva->id,
-            $returnUrl,
-            $finalUrl
-        );
-
-        $db_transaction->token = $response->token;
-        $db_transaction->save();
-        $reserva->status = Reserva::STATUS_WP_NORMAL_INIT_SUCCESS;
-        $reserva->save();
-
-        return view('pages.reserva-user', compact('reserva', 'responses','response'));
+        return view('pages.reserva-user', compact('reserva', 'responses'));
     }
 }
